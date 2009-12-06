3ed39be83bea1819e969e2c319e3f8b3bf01e155
alanruttenberg <alanruttenberg@3c59e798-7abc-11de-9755-51fe06bdd687>
2009-12-06 04:23:22 +0000

    add code to generate treeview

---
+++ trunk/src/tools/make-ogms-treeview.lisp
@@ -0,0 +1,13 @@
+(defun make-ogms-treeview (&optional
+			   (src (format nil "file://~a" (truename "ogms:ontology;ogms.owl")))
+			   (dest "ogms:web;ogms-treeml.xml")			   )
+  (let ((*inhibit-property-info-in-toolips* t))
+    (let ((kb (load-kb-jena src)))
+      (write-classtree-treeml kb :depth 10 :dont-show
+			      (list !span:SpatiotemporalRegion !snap:SpatialRegion
+				    !oboInOwl:ObsoleteClass
+				    !span:TemporalRegion
+				    )
+			      :root !bfo:Entity
+			      :fname dest
+			      :include-tooltips t))))
